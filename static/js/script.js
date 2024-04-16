cec2c837f4dec571e14b67d890365cba9bb3a0bb
Dev-Tonia <ogunwaanthonia@gmail.com>
2024-04-16 23:29:26 +0100

    added the footer and also make the favorite to be dynamic

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,17 @@
-const fav = document.querySelector(".fav1");
+const fav = document.querySelectorAll(".fav");
 let isClicked = false;
-fav.addEventListener("click", function () {
-  isClicked = !isClicked;
-  const heart = document.querySelector("i");
-  console.log(isClicked);
-  if (isClicked) {
-    heart.classList.remove("text-white");
-    heart.classList.add("text-danger");
-
-    console.log(heart.classList);
-  } else {
-    heart.classList.remove("text-danger");
-    heart.classList.add("text-white");
-  }
+fav.forEach(function (element) {
+  element.addEventListener("click", function () {
+    const heart = element.querySelector("i");
+    isClicked = !isClicked;
+    console.log(isClicked);
+    if (isClicked) {
+      heart.classList.remove("text-white");
+      heart.classList.add("text-danger");
+    } else {
+      heart.classList.remove("text-danger");
+      heart.classList.add("text-white");
+    }
+  });
 });
-
 // console.log(fav);
